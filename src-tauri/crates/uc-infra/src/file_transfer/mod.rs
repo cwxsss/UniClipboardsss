1f236db8b2a391ed55ad8bd3943e371d30ba0a5b
mkdir700 <mkdir700@gmail.com>
2026-04-17 13:04:17 +0800

    impl(uc-infra): add file transfer projection updater

--- src-tauri/crates/uc-infra/src/file_transfer/mod.rs
+++ src-tauri/crates/uc-infra/src/file_transfer/mod.rs
@@ -1,6 +1,8 @@
 mod event_store;
+mod projection;
 mod publisher;
 
 pub use event_store::InMemoryEventStore;
 pub use event_store::SqliteFileTransferEventStore;
+pub use projection::{ReceiverTransferContext, SqliteReceiverFileTransferProjectionUpdater};
 pub use publisher::InMemoryEventPublisher;
